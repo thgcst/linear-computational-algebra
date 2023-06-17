3176057464c3b3ba11e5e086671fd312b1c0b69f
Thiago <thiagodias2708@gmail.com>
2023-06-17 01:01:52 -0300

    feat: P2 task 2

--- P2/main.py
+++ P2/main.py
@@ -6,31 +6,38 @@ print("╔════════════╗")
 print("║   Task 1   ║")
 print("╚════════════╝")
 
+
 def run_exercise_1():
-  print("Exercício 1")
+    print("Exercício 1")
 
-  def f(x):
-    return math.log(math.cosh(x * math.sqrt(9.806 * 0.00341)), 10) - 50
+    def f(x):
+        return math.log(math.cosh(x * math.sqrt(9.806 * 0.00341)), 10) - 50
 
-  print("\tMétodo da bisseção: ",bissection_method(f, 0, 1000))
-  print("\tMétodo de Newton original: ",newton_method(f, 1000))
-  print("\tMétodo de Newton secante", secant_method(f, 1000))
-  print("\tMétodo da interpolação reversa", reverse_interpolation_method(f, 0, 100, 1000))
-  print("")
+    print("\tMétodo da bisseção: ", bissection_method(f, 0, 1000))
+    print("\tMétodo de Newton original: ", newton_method(f, 1000))
+    print("\tMétodo de Newton secante", secant_method(f, 1000))
+    print(
+        "\tMétodo da interpolação reversa",
+        reverse_interpolation_method(f, 0, 100, 1000),
+    )
+    print("")
+
 
 run_exercise_1()
 
+
 def run_exercise_2():
-  print("Exercício 2")
+    print("Exercício 2")
 
-  def f(x):
-    return 4*math.cos(x) - math.exp(2*x)
+    def f(x):
+        return 4 * math.cos(x) - math.exp(2 * x)
 
-  print("\tMétodo da bisseção: ", bissection_method(f, 2, 0))
-  print("\tMétodo de Newton original: ", newton_method(f, 0))
-  print("\tMétodo de Newton secante", secant_method(f, 0))
-  print("\tMétodo da interpolação reversa", reverse_interpolation_method(f, 0, 1, 10))
-  print("")
+    print("\tMétodo da bisseção: ", bissection_method(f, 2, 0))
+    print("\tMétodo de Newton original: ", newton_method(f, 0))
+    print("\tMétodo de Newton secante", secant_method(f, 0))
+    print("\tMétodo da interpolação reversa", reverse_interpolation_method(f, 0, 1, 10))
+    print("")
+
 
 run_exercise_2()
 
@@ -38,18 +45,88 @@ print("╔════════════╗")
 print("║   Task 2   ║")
 print("╚════════════╝")
 
+
 def run_exercise_5():
-  print("Exercício 5")
+    print("Exercício 5")
 
-  def f(x):
-    return 1 / (1 + x**2)
+    def f(x):
+        return 1 / (1 + x**2)
 
-  results, iterations, integration_points = adaptive_simpson_method(f, 0, 3).values()
+    results, iterations, integration_points = adaptive_simpson_method(f, 0, 3).values()
 
-  print("\tIterações: ", iterations)
-  print("\tPontos de integração: ", integration_points)
-  print("\tResultados: ", results)
-  print("")
-  
+    print("\tIterações: ", iterations)
+    print("\tPontos de integração: ", integration_points)
+    print("\tResultados: ", results)
+    print("")
+
 
 run_exercise_5()
+
+
+def run_exercise_6():
+    print("Exercício 6")
+
+    def S_sigma(x):
+        return 2 / ((1 - x**2) ** 2 + (x / 10) ** 2)
+
+    def m0(x):
+        return S_sigma(x)
+
+    def m2(x):
+        return x**2 * S_sigma(x)
+
+    simpson_results = adaptive_simpson_method(m0, 0, 10)["results"]
+    print("\tFunção m0 - Regra de Simpson: ", simpson_results[-1])
+
+    gauss_result = gauss_quadrature(m0, 0, 10, points=11)
+    print("\tFunção m0 - Método da Quadratura de Gauss (11 pontos): ", gauss_result)
+
+    simpson_results = adaptive_simpson_method(m2, 0, 10)["results"]
+    print("\tFunção m2 - Regra de Simpson: ", simpson_results[-1])
+
+    gauss_result = gauss_quadrature(m2, 0, 10, points=11)
+    print("\tFunção m2 - Método da Quadratura de Gauss (11 pontos): ", gauss_result)
+
+    print("")
+
+
+run_exercise_6()
+
+
+def run_exercise_7():
+    print("Exercício 7")
+
+    def S_n(x):
+        return (
+            4
+            * math.pi**3
+            * 3**2
+            * math.exp(-16 * math.pi**3 / (x**4 * 5**4))
+            / (x**5 * 5**4)
+        )
+
+    def S_sigma(x):
+        return 1 / ((1 - x**2) ** 2 + (x / 10) ** 2) * S_n(x)
+
+    def m0(x):
+        return S_sigma(x)
+
+    def m2(x):
+        return x**2 * S_sigma(x)
+
+    simpson_results = adaptive_simpson_method(m0, 0.01, 10)["results"]
+    print("\tFunção m0 - Regra de Simpson: ", simpson_results[-1])
+
+    gauss_result = gauss_quadrature(m0, 0.01, 10, points=11)
+    print("\tFunção m0 - Método da Quadratura de Gauss (11 pontos): ", gauss_result)
+
+    simpson_results = adaptive_simpson_method(m2, 0.01, 10)["results"]
+    print("\tFunção m2 - Regra de Simpson: ", simpson_results[-1])
+
+    gauss_result = gauss_quadrature(m2, 0.01, 10, points=11)
+    print("\tFunção m2 - Método da Quadratura de Gauss (11 pontos): ", gauss_result)
+
+    print("")
+
+
+run_exercise_7()
--- P2/tasks/task2.py
+++ P2/tasks/task2.py
@@ -1,21 +1,47 @@
+from P2.Gauss_Weights import Gauss_Weights
+
 TOL = 1e-5
-MAX_ITER = 10
+MAX_ITER = 20
+
 
 def simpson_method(function, a, b):
-  h = (b-a)/2
-  return h/3*(function(a) + 4.0*function((a+b)/2) + function(b))
+    h = (b - a) / 2
+    return h / 3 * (function(a) + 4.0 * function((a + b) / 2) + function(b))
+
 
 def adaptive_simpson_method(function, a, b):
-  results = []
-  for i in range(MAX_ITER):
-    intervals = 2**(i)
-    L = (b-a)/ intervals
+    results = []
+    for i in range(MAX_ITER):
+        intervals = 2 ** (i)
+        L = (b - a) / intervals
 
-    iteration_result = 0
-    for k in range(intervals):
-      iteration_result += simpson_method(function, a + k*L, a + (k+1)*L)
-    
-    if i > 0 and abs(iteration_result - results[-1])/abs(iteration_result) < TOL:
-      return {"results": results, "iterations": i+1, "integration_points": (intervals*2)+1}
-    else:
-      results.append(iteration_result)
+        iteration_result = 0
+        for k in range(intervals):
+            iteration_result += simpson_method(function, a + k * L, a + (k + 1) * L)
+
+        if i > 0 and abs(iteration_result - results[-1]) / abs(iteration_result) < TOL:
+            break
+        else:
+            results.append(iteration_result)
+
+    return {
+        "results": results,
+        "iterations": i + 1,
+        "integration_points": (intervals * 2) + 1,
+    }
+
+
+def gauss_quadrature(function, a, b, points=3):
+    def f(x):
+        def change_of_variable(x):
+            return 1 / 2 * ((b - a) * x + (b + a)) * (b - a) / 2
+
+        return function(change_of_variable(x))
+
+    result = 0
+    for i in range(points):
+        result += Gauss_Weights[points]["weights"][i] * f(
+            Gauss_Weights[points]["points"][i]
+        )
+
+    return result
